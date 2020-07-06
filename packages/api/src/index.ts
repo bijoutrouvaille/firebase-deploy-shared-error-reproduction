a806b944eaa048cdbf8d45b7ac1783ececa1c1f2
Bijou Trouvaille <bijoutrouvaille@gmail.com>
2020-07-05 20:45:52 -0500

    upgrade es5 to es2017

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,11 @@
 import functions = require('firebase-functions');
-import {SHARED_CONST} from '@root/shared/src/shared-test';
+import {SHARED_CONST} from '@root/shared/lib/shared-test';
 
 export const callMe = functions.https.onCall(async (data, ctx)=>{
   return {
     req: data,
     testingThings: 'HELLO!',
-    date: new Date()
+    date: new Date(),
+    x: SHARED_CONST
   }
 })
